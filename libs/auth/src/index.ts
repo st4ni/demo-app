c37d9fe9519fa3960a9751ccca5ba47703e7a266
c2904491_Stan_Fraser <stanfraser@gmail.com>
2022-05-18 22:40:41 +0100

    ngrx selectors 14 complete

--- libs/auth/src/index.ts
+++ libs/auth/src/index.ts
@@ -1,7 +1,8 @@
-export * from './lib/+state/auth.models';
-export * from './lib/+state/auth.selectors';
-export * from './lib/+state/auth.reducer';
 export * from './lib/+state/auth.actions';
+export * from './lib/+state/auth.effects';
+export * from './lib/+state/auth.reducer';
+export * from './lib/+state/auth.selectors';
+export * from './lib/+state/auth.models';
 export * from './lib/auth.module';
 export { AuthService } from './lib/services/auth/auth.service';
 export { AuthGuard } from './lib/guards/auth/auth.guard';
